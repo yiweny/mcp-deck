85a813e08fb8e114ea25b8802205da84e023e3f9
Yiwen Yuan <yiwen@x.ai>
2025-10-14 00:09:01 -0700

    fix startup

--- src/mcp_deck/server.py
+++ src/mcp_deck/server.py
@@ -1,14 +1,15 @@
 from fastmcp import FastMCP
-from fastmcp.tools import Tool
-from .db import get_engine
 from .loader import load_parquet_to_mysql
 from .tools.tools import get_user_purchase_history, get_details_of_list_of_products
 
 app = FastMCP("MCP Deck", version="0.1")
+
+
 @app.tool()
 def user_purchase_history(user_id: int):
     """Expose the get_user_purchase_history tool."""
     return get_user_purchase_history(user_id)
+
 
 @app.tool()
 def product_details(product_ids: list[int]):
@@ -22,4 +23,4 @@ if __name__ == "__main__":
     load_parquet_to_mysql()
 
     # Now start FastMCP
-    app.run(host="0.0.0.0", port=8000)
+    app.run(transport="http", host="0.0.0.0", port=8080)
